f9e7a8e562bbcd6418752da133cc705a10ad4029
samleishman <samuel.leishman@ionrev.com>
2022-08-17 12:10:59 -0600

    Refactor store.auth.js to use state, getters, actions

--- src/stores/store.auth.js
+++ src/stores/store.auth.js
@@ -1,8 +1,7 @@
 import { defineAuthStore, BaseModel } from 'feathers-pinia';
 // import { Users } from './services/users.js';
 
-import {lodash} from '@sparkz-community/common-client-lib';
-const {$lisNil} = lodash;
+import $lisNil from 'lodash/isNil';
 
 export class Auth extends BaseModel {
   constructor(data, options) {
@@ -17,6 +16,7 @@ export default async (
     state = () => ({}),
     getters = {},
     actions = {},
+    Users,
   } = {}) => {
 
   if ($lisNil(FeathersClient)) {
@@ -31,59 +31,54 @@ export default async (
     Model = extend_class_fn(Auth);
   }
 
-  const useStore = defineAuthStore({
+  return defineAuthStore({
     Model,
     clients: { api: feathersClient },
     userService: 'users',
-    state,
-    getters,
-    actions,
+    state() {
+      return {
+        userId: null,
+        rules: [],
+        logins: [],
+        activeLogin: {},
+        accounts: [],
+        activeAccount: {},
+        ...state(),
+      };
+    },
+    getters: {
+      authUser() {
+        return this.userId ? Users.getFromStore(this.userId) : null;
+      },
+
+      activeLogin(state, getters, rootState) {
+        if (!state.activeLogin) {
+          return null;
+        }
+        const {idField} = rootState['logins'];
+        const loginId = state.activeLogin[idField];
+        return rootState['logins'].keyedById[loginId] || null;
+      },
+      activeAccount(state, getters, rootState) {
+        if (!state.activeAccount) {
+          return null;
+        }
+        const {idField} = rootState['accounts'];
+        const accountId = state.activeAccount[idField];
+        return rootState['accounts'].keyedById[accountId] || null;
+      },
+
+      ...getters,
+    },
+    actions: {
+      handleResponse(response) {
+        this.payload = response || null;
+        this.userId = response.user.id || response.user._id;
+        Users.addToStore(response.user);
+        return response;
+      },
+
+      ...actions,
+    },
   });
-
-  return useStore;
 };
-
-// export default defineAuthStore({
-//   feathersClient,
-//   userService: 'users',
-//   state() {
-//     return {
-//       userId: null,
-//       rules: [],
-//       logins: [],
-//       activeLogin: {},
-//       accounts: [],
-//       activeAccount: {},
-//     };
-//   },
-//   getters: {
-//     authUser() {
-//       return this.userId ? Users.getFromStore(this.userId) : null;
-//     },
-//
-//     activeLogin(state, getters, rootState) {
-//       if (!state.activeLogin) {
-//         return null;
-//       }
-//       const {idField} = rootState['logins'];
-//       const loginId = state.activeLogin[idField];
-//       return rootState['logins'].keyedById[loginId] || null;
-//     },
-//     activeAccount(state, getters, rootState) {
-//       if (!state.activeAccount) {
-//         return null;
-//       }
-//       const {idField} = rootState['accounts'];
-//       const accountId = state.activeAccount[idField];
-//       return rootState['accounts'].keyedById[accountId] || null;
-//     },
-//   },
-//   actions: {
-//     handleResponse(response) {
-//       this.payload = response || null;
-//       this.userId = response.user.id || response.user._id;
-//       Users.addToStore(response.user);
-//       return response;
-//     },
-//   },
-// });
