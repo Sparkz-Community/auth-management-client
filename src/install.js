5514f908dbe3fefacca5a1c0e2218f7e21ece2bd
dallinjohnson <dallin.b.johnson@gmail.com>
2022-08-23 11:24:12 -0600

    Add globalProperties and global provide for useAuthStore.

--- src/install.js
+++ src/install.js
@@ -2,7 +2,9 @@ import {FormGenClientLib} from './packages';
 
 import * as components from './components';
 
-const install = (app, {prefix, loadFormGen = true, loadComponents = false} = {}) => {
+const install = (app, {prefix, useAuthStore, loadFormGen = true, loadComponents = false} = {}) => {
+  if (!useAuthStore) throw new Error('useAuthStore is required');
+
   if (loadFormGen) {
     app.use(FormGenClientLib);
   }
@@ -13,6 +15,9 @@ const install = (app, {prefix, loadFormGen = true, loadComponents = false} = {})
       app.component(_key, components[key]);
     }
   }
+
+  app.config.globalProperties.$useAuthStore = useAuthStore;
+  app.provide('$useAuthStore', useAuthStore);
 };
 
 // auto install
